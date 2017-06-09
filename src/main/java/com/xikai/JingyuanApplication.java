9ee7b29a09f5d1e43ea5488ca72279ad75839a41
xiongxikai <xx64@njit.edu>
2017-06-09 19:43:25 -0400

    login app full stack

---
+++ src/main/java/com/xikai/JingyuanApplication.java
@@ -0,0 +1,12 @@
+package com.xikai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingyuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingyuanApplication.class, args);
+	}
+}
---
+++ src/main/java/com/xikai/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.xikai.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by xikaixiong on 6/9/17.
+ */
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        //in templates html file name is the same with the return string value
+        return "hello";
+    }
+
+}
